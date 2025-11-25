73ff44b6cd5a2a9fe5f1a981347172b81f4b955a
Kenneth Tkach <ktkach.dev@gmail.com>
2025-11-24 19:55:36 -0500

    Main formatting edits

--- src/main.java
+++ src/main.java
@@ -1,6 +1,11 @@
-import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.Set;
+/* 
+We are in charge of designing a system to install software packages. 
+We are required to support the installation of a package and all of its 
+dependencies. Here is an example of a package structure that we would 
+need to install: A depends on B, C B depends on D, E, F C depends on F F 
+depends on G Write code to implement installing package A along with its
+ dependencies 
+*/ 
 
 public class main {
     public static void main(String[] args) {
@@ -13,18 +18,9 @@ public class main {
     	tests.testDepBlockedUninstall();
     	tests.testAlreadyInstalled();
 
-
     }
 }
 
-/* 
-We are in charge of designing a system to install software packages. 
-We are required to support the installation of a package and all of its 
-dependencies. Here is an example of a package structure that we would 
-need to install: A depends on B, C B depends on D, E, F C depends on F F 
-depends on G Write code to implement installing package A along with its
- dependencies 
-*/ 
 
 
 
